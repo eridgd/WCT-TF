70fbb1fa081bd87b746b99d9ef65d859bbfb192d
edavis <eridgdavis@gmail.com>
2017-09-21 18:59:31 -0400

    Pre-trained model download

---
+++ models/download_models.sh
@@ -0,0 +1,10 @@
+cd models
+# The VGG-19 network is obtained by:
+# 1. converting vgg_normalised.caffemodel to .t7 using loadcaffe
+# 2. inserting a convolutional module at the beginning to preprocess the image
+# 3. replacing zero-padding with reflection-padding
+# The original vgg_normalised.caffemodel can be obtained with:
+# "wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel"
+wget -c -O models.zip "https://www.dropbox.com/s/5wf33k9v0t1anj9/models.zip?dl=1"
+unzip models.zip
+cd .. 
