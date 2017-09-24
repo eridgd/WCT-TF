9e4cde64b80f1acd8b16d25b3a3651058715e528
edavis <eridgdavis@gmail.com>
2017-09-24 16:13:20 -0400

    Update model download

--- models/download_models.sh
+++ models/download_models.sh
@@ -1,10 +1,5 @@
+# Download pre-trained models for relu5_1 relu4_1 relu3_1 relu2_1 relu1_1
 cd models
-# The VGG-19 network is obtained by:
-# 1. converting vgg_normalised.caffemodel to .t7 using loadcaffe
-# 2. inserting a convolutional module at the beginning to preprocess the image
-# 3. replacing zero-padding with reflection-padding
-# The original vgg_normalised.caffemodel can be obtained with:
-# "wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel"
 wget -c -O models.zip "https://www.dropbox.com/s/ssg39coiih5hjzz/models.zip?dl=1"
 unzip models.zip
 cd .. 
--- models/download_vgg.sh
+++ models/download_vgg.sh
@@ -1,9 +1,10 @@
-cd models
+# Borrowed from https://github.com/xunhuang1995/AdaIN-style/
 # The VGG-19 network is obtained by:
 # 1. converting vgg_normalised.caffemodel to .t7 using loadcaffe
 # 2. inserting a convolutional module at the beginning to preprocess the image
 # 3. replacing zero-padding with reflection-padding
 # The original vgg_normalised.caffemodel can be obtained with:
 # "wget -c --no-check-certificate https://bethgelab.org/media/uploads/deeptextures/vgg_normalised.caffemodel"
+cd models
 wget -c -O vgg_normalised.t7 "https://www.dropbox.com/s/kh8izr3fkvhitfn/vgg_normalised.t7?dl=1"
 cd .. 
